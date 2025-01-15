fb2b2cbdd9c3e9d62e93ceaefc9954aafa251f1c
Long-Brian-Yang <longyang2000110@gmail.com>
2025-01-15 12:34:03 +0900

    updated

---
+++ deepmd-kit_tsubame_tutorial/ch4.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#$ -l h_rt=6:00:00
+#$ -l node_f=1
+#$ -cwd
+
+
+module purge
+module load deepmd-kit lammps
+module li 2>&1
+
+# enable DeePMD-kit for lammps/2aug2023_u3
+export LAMMPS_PLUGIN_PATH=$DEEPMD_KIT_DIR/lib/deepmd_lmp
+
+# https://tutorials.deepmodeling.com/en/latest/Tutorials/DeePMD-kit/learnDoc/Handson-Tutorial%28v2.0.3%29.html
+
+wget https://dp-public.oss-cn-beijing.aliyuncs.com/community/CH4.tar
+tar xf CH4.tar
+
+cd CH4/00.data
+python3 <<EOF
+import dpdata
+import numpy as np
+data = dpdata.LabeledSystem('OUTCAR', fmt = 'vasp/outcar')
+print('# the data contains %d frames' % len(data))
+# random choose 40 index for validation_data
+index_validation = np.random.choice(200,size=40,replace=False)
+# other indexes are training_data
+index_training = list(set(range(200))-set(index_validation))
+data_training = data.sub_system(index_training)
+data_validation = data.sub_system(index_validation)
+# all training data put into directory:"training_data"
+data_training.to_deepmd_npy('training_data')
+# all validation data put into directory:"validation_data"
+data_validation.to_deepmd_npy('validation_data')
+print('# the training data contains %d frames' % len(data_training))
+print('# the validation data contains %d frames' % len(data_validation))
+EOF
+
+
+cd ../01.train
+dp train input.json
+dp freeze -o graph.pb
+dp compress -i graph.pb -o graph-compress.pb
+dp test -m graph-compress.pb -s ../00.data/validation_data -n 40 -d results
+
+cd ../02.lmp
+ln -s ../01.train/graph-compress.pb
+mpirun -x PATH -x LD_LIBRARY_PATH -np 4 lmp -sf gpu -in in.lammps
